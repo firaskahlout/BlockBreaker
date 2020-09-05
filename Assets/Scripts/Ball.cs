5eea36b9f3d56d35517c30d076767720daa1e55c
firaskahlout <firaskahlout@outlook.com>
2020-09-05 14:24:53 +0300

    added game files with screenshots

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPath = 2f;
+    [SerializeField] float yPath = 15f;
+    [SerializeField] AudioClip[] ballSounds ;
+
+    //state
+    Vector2 paddleToBallVector;
+
+
+    // Cached component reference
+    AudioSource myAudioSource;
+
+
+	// Use this for initialization
+	void Start () {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+	}
+    bool hasStarted = false;
+    // Update is called once per frame
+    void Update ()
+    {
+        if(!hasStarted){
+
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+
+    }
+    private void LaunchOnMouseClick(){
+        if(Input.GetMouseButtonDown(0)){
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPath , yPath);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(hasStarted){
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0,ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+
+    Level level;
+
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        if(tag == "Breakable"){
+            level.CountBlocks();
+        }
+
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            PlayBBlockDestroySFX();
+            Destroy(gameObject);
+            level.BlockDestroyed();
+            TriggerSparklesVFX();
+        }
+
+    }
+
+    private void PlayBBlockDestroySFX()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+    }
+
+    private void TriggerSparklesVFX(){
+
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position , transform.rotation);
+
+    }
+
+}
